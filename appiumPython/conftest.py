7c06dd9f161c20933f904f83171c2c8e7fa3d031
Waldo <w.b.g.serrano@gmail.com>
2025-11-22 12:14:58 -0300

    new

---
+++ appiumPython/conftest.py
@@ -0,0 +1,41 @@
+import pytest
+from appium import webdriver
+from appium.options.android import UiAutomator2Options
+import time
+import datetime
+
+@pytest.fixture
+def driver():
+    options = UiAutomator2Options().load_capabilities({
+        "platformName": "Android",
+        "automationName": "UiAutomator2",
+        #"deviceName": "Xiaomi",
+        #"udid": "scso7txwh6lrjbqs",
+        "deviceName": "Xiaomi-Wifi",
+        #"udid": "adb-scso7txwh6lrjbqs-xPSg8r._adb-tls-connect._tcp", 
+        #"udid":"emulator-5554"
+        "appPackage": "com.miui.calculator",
+        "appActivity": "com.miui.calculator.cal.CalculatorActivity",
+        "noReset": True,
+        "ignoreHiddenApiPolicyError": True,
+        "autoGrantPermissions": True
+    })
+
+    driver = webdriver.Remote("http://127.0.0.1:4723", options=options)
+    time.sleep(2)
+    yield driver
+    driver.quit()
+
+# Se ejecuta AUTOMÁTICAMENTE cuando un test falla
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    result = outcome.get_result()
+
+    if result.when == "call" and result.failed:
+        driver = item.funcargs.get("driver")
+        if driver:
+            timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+            nombre = f"FAILED_{item.name}_{timestamp}.png"
+            driver.save_screenshot(nombre)
+            print(f"[ERROR] Screenshot guardado: {nombre}")
--- appiumPython/xiaomiAllure.py
+++ appiumPython/xiaomiAllure.py
@@ -1,52 +1,79 @@
+import allure
 from selenium.webdriver.common.by import By
-import time
+
 
 def limpiar(driver):
     driver.find_element(By.ID, "com.miui.calculator:id/btn_c_s").click()
-    time.sleep(1)
+
 
 def obtener_resultado(driver):
     resultado = driver.find_element(By.ID, "com.miui.calculator:id/result").text
     return resultado.replace("=", "").replace(" ", "").replace(".", "").strip()
 
+
+@allure.feature("Operaciones")
+@allure.story("Suma")
 def test_suma(driver):
-    limpiar(driver)
-    driver.find_element(By.ID, "com.miui.calculator:id/btn_2_s").click()
-    driver.find_element(By.ID, "com.miui.calculator:id/btn_plus_s").click()
-    driver.find_element(By.ID, "com.miui.calculator:id/btn_5_s").click()
-    driver.find_element(By.ID, "com.miui.calculator:id/btn_equal_s").click()
+    with allure.step("Limpiar calculadora"):
+        limpiar(driver)
+
+    with allure.step("Realizar operación 2 + 5"):
+        driver.find_element(By.ID, "com.miui.calculator:id/btn_2_s").click()
+        driver.find_element(By.ID, "com.miui.calculator:id/btn_plus_s").click()
+        driver.find_element(By.ID, "com.miui.calculator:id/btn_5_s").click()
+        driver.find_element(By.ID, "com.miui.calculator:id/btn_equal_s").click()
+
     assert obtener_resultado(driver) == "7"
 
+
+@allure.feature("Operaciones")
+@allure.story("Multiplicación")
 def test_multiplicacion(driver):
     limpiar(driver)
+
     driver.find_element(By.ID, "com.miui.calculator:id/btn_3_s").click()
     driver.find_element(By.ID, "com.miui.calculator:id/btn_mul_s").click()
     driver.find_element(By.ID, "com.miui.calculator:id/btn_4_s").click()
     driver.find_element(By.ID, "com.miui.calculator:id/btn_equal_s").click()
+
     assert obtener_resultado(driver) == "12"
 
+
+@allure.feature("Operaciones")
+@allure.story("División")
 def test_division(driver):
     limpiar(driver)
+
     driver.find_element(By.ID, "com.miui.calculator:id/btn_8_s").click()
     driver.find_element(By.ID, "com.miui.calculator:id/btn_div_s").click()
     driver.find_element(By.ID, "com.miui.calculator:id/btn_2_s").click()
     driver.find_element(By.ID, "com.miui.calculator:id/btn_equal_s").click()
+
     assert obtener_resultado(driver) == "4"
 
-def test_RestaMala(driver):
+
+@allure.feature("Operaciones")
+@allure.story("DivisiónMala")
+def test_divisionMala(driver):
     limpiar(driver)
+
     driver.find_element(By.ID, "com.miui.calculator:id/btn_8_s").click()
+    driver.find_element(By.ID, "com.miui.calculator:id/btn_div_s").click()
+    driver.find_element(By.ID, "com.miui.calculator:id/btn_2_s").click()
+    driver.find_element(By.ID, "com.miui.calculator:id/btn_equal_s").click()
+
+    # Este va a fallar y sacará screenshot automáticamente
+    assert obtener_resultado(driver) == "444"
+
+
+@allure.feature("Operaciones")
+@allure.story("Resta")
+def test_resta(driver):
+    limpiar(driver)
+
+    driver.find_element(By.ID, "com.miui.calculator:id/btn_5_s").click()
     driver.find_element(By.ID, "com.miui.calculator:id/btn_minus_s").click()
     driver.find_element(By.ID, "com.miui.calculator:id/btn_2_s").click()
     driver.find_element(By.ID, "com.miui.calculator:id/btn_equal_s").click()
-    assert obtener_resultado(driver) == "4"  # Este fallará y sacará screenshot
 
-def test_Resta(driver):
-    limpiar(driver)
-    driver.find_element(By.ID, "com.miui.calculator:id/btn_8_s").click()
-    driver.find_element(By.ID, "com.miui.calculator:id/btn_minus_s").click()
-    driver.find_element(By.ID, "com.miui.calculator:id/btn_2_s").click()
-    driver.find_element(By.ID, "com.miui.calculator:id/btn_equal_s").click()
-    assert obtener_resultado(driver) == "6"
-
-#com.miui.calculator:id/btn_percent_s
+    assert obtener_resultado(driver) == "3"
